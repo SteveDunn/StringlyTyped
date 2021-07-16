3f408233d06b569a166ac541f4f09a721d7feeec
Steve Dunn <steve@dunnhq.com>
2021-07-16 12:37:22 +0100

    Added samples

---
+++ samples/StringlyTyped.Examples/ValidationExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StringlyTyped.Examples
+{
+    internal static class ValidationExample
+    {
+        public static void Run()
+        {
+            string[] names = new[] { "Dave Grohl", "David Beckham", "Fred Flintstone" };
+
+            var processor = new DaveProcessor();
+
+            foreach (string name in names)
+            {
+                try
+                {
+                    processor.Process(Dave.From(name));
+                }
+                catch(ValueObjectValidationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+
+    public class Dave : ValueObject<string, Dave>
+    {
+        public override string ValidationErrors() => Value.StartsWith("dave ", StringComparison.OrdinalIgnoreCase) ||
+                                                     Value.StartsWith("david ", StringComparison.OrdinalIgnoreCase)
+            ? ""
+            : $"must be a dave or david - {Value} is neither.";
+    }
+
+    internal class DaveProcessor
+    {
+        internal void Process(Dave dave) => Console.WriteLine($"Processing {dave}");
+    }
+}
